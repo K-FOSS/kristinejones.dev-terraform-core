bb24949a7cd70dac6c951c0bc7317f7aaf369ae9
Kristian Jones <me@kristianjones.dev>
2021-07-25 21:39:37 +0000

    fix: Add required providers in each level

--- Keycloak/Realms/KJDev/UserFederation/OpenLDAP/main.tf
+++ Keycloak/Realms/KJDev/UserFederation/OpenLDAP/main.tf
@@ -1,6 +1,29 @@
 #
 # OpenLDAP User Federation
 #
+terraform {
+  required_providers {
+    mysql = {
+      source = "winebarrel/mysql"
+      version = "1.10.4"
+    }
+
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.14.0"
+    }
+
+    vault = {
+      source = "hashicorp/vault"
+      version = "2.22.1"
+    }
+
+    keycloak = {
+      source  = "mrparkers/keycloak"
+      version = "3.2.1"
+    }
+  }
+}
 
 data "vault_generic_secret" "openldap" {
   path = "secret/keycloak/OPENLDAP"
--- Keycloak/Realms/KJDev/main.tf
+++ Keycloak/Realms/KJDev/main.tf
@@ -1,3 +1,27 @@
+terraform {
+  required_providers {
+    mysql = {
+      source = "winebarrel/mysql"
+      version = "1.10.4"
+    }
+
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.14.0"
+    }
+
+    vault = {
+      source = "hashicorp/vault"
+      version = "2.22.1"
+    }
+
+    keycloak = {
+      source  = "mrparkers/keycloak"
+      version = "3.2.1"
+    }
+  }
+}
+
 resource "keycloak_realm" "kjdev" {
   realm             = "KJDev"
   enabled           = true
