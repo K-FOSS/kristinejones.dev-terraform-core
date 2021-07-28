ac30cd45b09deb795c87341bdf8f5328fa58bd3a
Kristian Jones <me@kristianjones.dev>
2021-07-28 07:25:34 +0000

    feat: Use Vault Token... From vault

--- Hashicorp/Consul/main.tf
+++ Hashicorp/Consul/main.tf
@@ -32,13 +32,17 @@ provider "consul" {
   scheme = "https"
 }
 
-# resource "consul_certificate_authority" "connect" {
-#   connect_provider = "vault"
+data "vault_generic_secret" "vault" {
+  path = "keycloak/VAULT"
+}
 
-#   config = {
-#     address = "https://vault.kristianjones.dev:443"
-#     token = "${vault_token.consulConnectCAToken.client_token}"
-#     root_pki_path = "connect_root/cert/0d-f6-61-17-3c-cc-35-cd-8f-dd-85-5c-aa-ea-7c-6e-ee-78-30-ed"
-#     intermediate_pki_path = "connect_inter"
-#   }
-# }
+resource "consul_certificate_authority" "connect" {
+  connect_provider = "vault"
+
+  config = {
+    address = "https://vault.kristianjones.dev:443"
+    token = "${data.vault_generic_secret.vault.data["TOKEN"]}"
+    root_pki_path = "connect_root"
+    intermediate_pki_path = "connect_inter"
+  }
+}
