0e6bcf2ade0bf9b3606e97e87eea3672d10b4029
Kristian Jones <me@kristianjones.dev>
2021-07-28 11:25:07 +0000

    feat: Let's give this another shot

--- Docker/main.tf
+++ Docker/main.tf
@@ -65,100 +65,99 @@ resource "docker_image" "mariadb" {
   keep_locally = true
 }
 
-# resource "docker_service" "DHCPDatabase" {
-#   name = "DHCPDatabase"
+resource "docker_service" "DHCPDatabase" {
+  name = "dhcpDatabase"
 
-#   task_spec {
-#     container_spec {
-#       image = "mariadb:10"
+  task_spec {
+    container_spec {
+      image = "mariadb:10"
 
-#       hostname = "DHCPDatabase"
+      hostname = "dhcpDatabase"
 
-#       env = {
-#         MYSQL_ROOT_PASSWORD = "password"
-#         MYSQL_DATABASE = "DHCP"
-#         MYSQL_USER = "dhcp"
-#         MYSQL_PASSWORD = "password"
-#         MYSQL_ROOT_HOST = "DHCPDatabase"
-#       }
+      env = {
+          MYSQL_ROOT_PASSWORD = "password/"
+          MYSQL_DATABASE = "DHCP"
+          MYSQL_USER = "dhcp"
+          MYSQL_PASSWORD = "password"
+        }
 
-#       mounts {
-#         target    = "/var/lib/mysql"
-#         source    = "dhcp-database"
-#         type      = "volume"
-#       }
-#     }
+      mounts {
+        target    = "/var/lib/mysql"
+        source    = "${var.NextCloudBucket.bucket}"
+        type      = "volume"
+      }
+    }
 
-#     networks = ["${data.docker_network.storageIntWeb.id}"]
-#   }
-# }
-
-resource "docker_container" "DHCPDatabase" {
-  name    = "dhcpDatabase"
-  image   = "mariadb:10"
-
-  cpu_shares      = 4
-  memory = 256
-
-  dns        = ["1.1.1.1", "1.0.0.1"]
-
-  log_driver = "json-file"
-
-  networks_advanced {
-    name = data.docker_network.storageIntWeb.id
-
-    aliases = ["DHCPMariaDB"]
+    networks = ["${data.docker_network.storageIntWeb.id}"]
   }
-
-  volumes {
-    volume_name    = "${var.NextCloudBucket.bucket}"
-    container_path = "/var/lib/mysql"
-    read_only      = false
-  }
-
-  env = [
-    "MYSQL_ROOT_PASSWORD=password/",
-    "MYSQL_DATABASE=DHCP",
-    "MYSQL_USER=dhcp",
-    "MYSQL_PASSWORD=password"
-  ]
-
-  # lifecycle {
-  #   ignore_changes = [
-  #     # Ignore changes to tags, e.g. because a management agent
-  #     # updates these based on some ruleset managed elsewhere.
-  #     command,
-  #     cpu_shares,
-  #     dns,
-  #     dns_opts,
-  #     dns_search,
-  #     entrypoint,
-  #     exit_code,
-  #     gateway,
-  #     group_add,
-  #     hostname,
-  #     init,
-  #     ip_address,
-  #     ip_prefix_length,
-  #     ipc_mode,
-  #     links,
-  #     log_opts,
-  #     max_retry_count,
-  #     memory,
-  #     memory_swap,
-  #     network_data,
-  #     network_mode,
-  #     privileged,
-  #     publish_all_ports,
-  #     security_opts,
-  #     shm_size,
-  #     sysctls,
-  #     tmpfs,
-  #     healthcheck,
-  #     labels,
-  #   ]
-  # }
 }
+
+# resource "docker_container" "DHCPDatabase" {
+#   name    = "dhcpDatabase"
+#   image   = "mariadb:10"
+
+#   cpu_shares      = 4
+#   memory = 256
+
+#   dns        = ["1.1.1.1", "1.0.0.1"]
+
+#   log_driver = "json-file"
+
+#   networks_advanced {
+#     name = data.docker_network.storageIntWeb.id
+
+#     aliases = ["DHCPMariaDB"]
+#   }
+
+#   volumes {
+#     volume_name    = "${var.NextCloudBucket.bucket}"
+#     container_path = "/var/lib/mysql"
+#     read_only      = false
+#   }
+
+#   env = [
+#     "MYSQL_ROOT_PASSWORD=password/",
+#     "MYSQL_DATABASE=DHCP",
+#     "MYSQL_USER=dhcp",
+#     "MYSQL_PASSWORD=password"
+#   ]
+
+#   # lifecycle {
+#   #   ignore_changes = [
+#   #     # Ignore changes to tags, e.g. because a management agent
+#   #     # updates these based on some ruleset managed elsewhere.
+#   #     command,
+#   #     cpu_shares,
+#   #     dns,
+#   #     dns_opts,
+#   #     dns_search,
+#   #     entrypoint,
+#   #     exit_code,
+#   #     gateway,
+#   #     group_add,
+#   #     hostname,
+#   #     init,
+#   #     ip_address,
+#   #     ip_prefix_length,
+#   #     ipc_mode,
+#   #     links,
+#   #     log_opts,
+#   #     max_retry_count,
+#   #     memory,
+#   #     memory_swap,
+#   #     network_data,
+#   #     network_mode,
+#   #     privileged,
+#   #     publish_all_ports,
+#   #     security_opts,
+#   #     shm_size,
+#   #     sysctls,
+#   #     tmpfs,
+#   #     healthcheck,
+#   #     labels,
+#   #   ]
+#   # }
+# }
 
 # resource "docker_service" "postgresDatabase" {
 #   name = "postgres-database"
