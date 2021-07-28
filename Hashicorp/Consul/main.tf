ff7e50b162dc0e0abc6562e4b165c77e7bdbff1a
Kristian Jones <me@kristianjones.dev>
2021-07-28 07:37:41 +0000

    fix: Undo

--- Hashicorp/Consul/main.tf
+++ Hashicorp/Consul/main.tf
@@ -36,13 +36,13 @@ data "vault_generic_secret" "vault" {
   path = "keycloak/VAULT"
 }
 
-resource "consul_certificate_authority" "connect" {
-  connect_provider = "vault"
+# resource "consul_certificate_authority" "connect" {
+#   connect_provider = "vault"
 
-  config = {
-    address = "https://vault.kristianjones.dev:443"
-    token = "${data.vault_generic_secret.vault.data["TOKEN"]}"
-    root_pki_path = "connect_root"
-    intermediate_pki_path = "connect_inter"
-  }
-}
+#   config = {
+#     address = "https://vault.kristianjones.dev:443"
+#     token = "${data.vault_generic_secret.vault.data["TOKEN"]}"
+#     root_pki_path = "connect_root"
+#     intermediate_pki_path = "connect_inter"
+#   }
+# }
