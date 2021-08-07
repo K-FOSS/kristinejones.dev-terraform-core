653e4765e6b1518d44259c1bdf285c133d4e9097
Kristian Jones <me@kristianjones.dev>
2021-08-07 10:21:32 +0000

    fix: Add Docker provider to main

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "2.12.0"
     }
 
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.14.0"
+    }
+
     mysql = {
       source = "winebarrel/mysql"
       version = "1.10.4"
