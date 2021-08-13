3b864bb8ae79cc4583d0ed906b1c9f61d580e5c0
Kristian Jones <me@kristianjones.dev>
2021-08-13 14:16:47 +0000

    fix; IDK

--- Insights/Grafana/main.tf
+++ Insights/Grafana/main.tf
@@ -14,27 +14,27 @@ provider "grafana" {
   org_id = 1
 }
 
-resource "grafana_data_source" "prometheus" {
-  type = "prometheus"
-  name = "prometheus"
-  url  = "http://Prometheus:9090"
+# resource "grafana_data_source" "prometheus" {
+#   type = "prometheus"
+#   name = "prometheus"
+#   url  = "http://Prometheus:9090"
 
-  access_mode = "proxy"
+#   access_mode = "proxy"
 
-  #is_default = true
+#   #is_default = true
 
-  json_data {
-		http_method = "GET"
-		query_timeout = "1"
-	}
-}
+#   json_data {
+# 		http_method = "GET"
+# 		query_timeout = "1"
+# 	}
+# }
 
-resource "grafana_data_source" "Loki" {
-  type = "loki"
-  name = "loki"
-  url  = "http://tasks.LokiGateway:8080"
+# resource "grafana_data_source" "Loki" {
+#   type = "loki"
+#   name = "loki"
+#   url  = "http://tasks.LokiGateway:8080"
 
-  access_mode = "proxy"
+#   access_mode = "proxy"
 
-  #is_default = true
-}
+#   #is_default = true
+# }
