6ec25eacceab92f3adb2cbfdd68dec058ae819cf
Kristian Jones <me@kristianjones.dev>
2021-08-13 18:00:35 +0000

    feat: Attempt to run StorkAgent in Kea DHCP

--- Docker/Configs/DHCP/entry.sh
+++ Docker/Configs/DHCP/entry.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+/usr/sbin/stork-agent register --server-url http://StorkServer:8080  --agent-address $HOSTNAME:8080
+
+
 /usr/sbin/keactrl start -c /config/keactrl.conf
+
+/usr/sbin/stork-agent start --server-url http://StorkServer:8080  --agent-address $HOSTNAME:8080
 
 exec watch -n 5 /usr/sbin/keactrl status
