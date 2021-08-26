d31d1c0dcb7a2847987f8a7040477ede8fa45531
Kristian Jones <me@kristianjones.dev>
2021-08-26 11:01:01 +0000

    feat(COnsul);

--- Docker/Services/Consul/main.tf
+++ Docker/Services/Consul/main.tf
@@ -255,6 +255,7 @@ resource "docker_service" "ConsulIngressProxy" {
         CONSUL_CLIENT_INTERFACE = "eth0"
         CONSUL_HTTP_ADDR = "tasks.Consul:8500"
         CONSUL_GRPC_ADDR = "tasks.Consul:8502"
+        CONSUL_HTTP_TOKEN = "e95b599e-166e-7d80-08ad-aee76e7ddf19"
       }
 
       # dir    = "/root"
