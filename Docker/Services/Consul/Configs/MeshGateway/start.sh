a1226d68def33996a7030fc2546c910394b69242
Kristian Jones <me@kristianjones.dev>
2021-08-26 13:16:10 +0000

    fsdfs

--- Docker/Services/Consul/Configs/MeshGateway/start.sh
+++ Docker/Services/Consul/Configs/MeshGateway/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-/entrypoint.sh consul connect envoy -gateway=mesh -register -service=gateway-primary -address=${MESH_HOST}:8443 -bind-address=${MESH_HOST}=0.0.0.0:8443 -token=fb3772dd-a44b-2428-971c-c67f321fdcac
+/entrypoint.sh consul connect envoy -gateway=mesh -register -address=${MESH_HOST}:8443 -bind-address=${MESH_HOST}=0.0.0.0:8443 -token=e95b599e-166e-7d80-08ad-aee76e7ddf19
