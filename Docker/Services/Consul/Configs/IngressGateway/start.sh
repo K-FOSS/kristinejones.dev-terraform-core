aad8f822b8b5d1eb418d8ffacd2c9906a3d0a8d6
Kristian Jones <me@kristianjones.dev>
2021-08-27 04:34:11 +0000

    fix: Fix Interface used

--- Docker/Services/Consul/Configs/IngressGateway/start.sh
+++ Docker/Services/Consul/Configs/IngressGateway/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-/entrypoint.sh consul connect envoy -gateway=ingress -register -service=${SERVICE_NAME} -address '{{ GetInterfaceIP "eth0" }}:8888' -bind-address=${INGRESS_HOST}=0.0.0.0:8888 -token=e95b599e-166e-7d80-08ad-aee76e7ddf19
+/entrypoint.sh consul connect envoy -gateway=ingress -register -service=${SERVICE_NAME} -address '{{ GetInterfaceIP "eth1" }}:8888' -bind-address=${INGRESS_HOST}=0.0.0.0:8888 -token=e95b599e-166e-7d80-08ad-aee76e7ddf19
