48859d4c03d4fd519db7d9b9a2c835865d0a3b10
Kristian Jones <me@kristianjones.dev>
2021-09-03 21:55:42 +0000

    fi

--- Docker/Services/Consul/Configs/Consul/start.sh
+++ Docker/Services/Consul/Configs/Consul/start.sh
@@ -3,15 +3,15 @@ RETRY_JOIN=""
 
 case ${CONSUL_HOST} in
      "Consul1" )
-           RETRY_JOIN="-bootstrap -retry-join Consul2 -retry-join Consul3"
+           RETRY_JOIN="-retry-join Consul2 -retry-join Consul3"
            echo "Consul1 means Consul2 Consul3"
            ;;
      "Consul2" )
-           RETRY_JOIN="-bootstrap-expect=3 -retry-join Consul1 -retry-join Consul3"
+           RETRY_JOIN="-retry-join Consul1 -retry-join Consul3"
            echo "Consul2 means Consul1 Consul3"
            ;;
      "Consul3" )
-           RETRY_JOIN="-bootstrap-expect=3 -retry-join Consul1 -retry-join Consul2"
+           RETRY_JOIN="retry-join Consul1 -retry-join Consul2"
            echo "Consul3 means Consul1 Consul2"
            ;;
      * )
@@ -19,4 +19,4 @@ case ${CONSUL_HOST} in
            ;;
 esac
 
-/usr/local/bin/docker-entrypoint.sh agent -server -bind 0.0.0.0  ${RETRY_JOIN} -advertise '{{ GetInterfaceIP "eth0" }}' -node ${CONSUL_HOST} -disable-host-node-id -config-format=json -data-dir=/Data -config-file=/Config/Config.json
+/usr/local/bin/docker-entrypoint.sh agent -server ${RETRY_JOIN} -node=${CONSUL_HOST} -disable-host-node-id -config-format=json -data-dir=/Data -config-file=/Config/Config.json -bootstrap-expect=3
