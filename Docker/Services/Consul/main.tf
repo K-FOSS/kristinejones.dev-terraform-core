265bee76a3ad823aec422977be66cbcd25eb95fc
Kristian Jones <me@kristianjones.dev>
2021-09-10 17:42:37 +0000

    fix: int bind

--- Docker/Services/Consul/main.tf
+++ Docker/Services/Consul/main.tf
@@ -151,9 +151,9 @@ resource "docker_service" "ConsulAgent" {
       hostname = "ConsulAgent{{.Task.Slot}}"
 
       env = {
-        CONSUL_BIND_INTERFACE = "eth0"
-        CONSUL_CLIENT_INTERFACE = "eth0"
-        CONSUL_HOST = "Consul{{.Task.Slot}}"
+        CONSUL_BIND_INTERFACE = "eth1"
+        CONSUL_CLIENT_INTERFACE = "eth1"
+        CONSUL_HOST = "ConsulAgent{{.Task.Slot}}"
         NODE_HOST = "{{.Node.Hostname}}.vps1.kristianjones.dev"
       }
 
