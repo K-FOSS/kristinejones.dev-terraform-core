ec38d89026f335043363ad8abd26b5d81bf8ed54
Kristian Jones <me@kristianjones.dev>
2021-09-14 10:11:07 +0000

    fix: Stuff & Things

--- Docker/Services/Consul/main.tf
+++ Docker/Services/Consul/main.tf
@@ -527,8 +527,6 @@ resource "consul_config_entry" "ProxyDefaults" {
       local_connect_timeout_ms = 1000
       handshake_timeout_ms     = 10000
 
-      envoy_prometheus_bind_addr = "0.0.0.0:9100"
-
       envoy_dns_discovery_type = "STRICT_DNS"
     }
   })
