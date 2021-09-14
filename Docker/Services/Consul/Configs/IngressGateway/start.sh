14b2144d5faf40073a9fa40199e1e43e3384dcbd
Kristian Jones <me@kristianjones.dev>
2021-09-14 17:36:14 +0000

    fix: stuff

--- Docker/Services/Consul/Configs/IngressGateway/start.sh
+++ Docker/Services/Consul/Configs/IngressGateway/start.sh
@@ -2,15 +2,15 @@
 
 case ${NODE_HOST} in
      "node1.vps1.kristianjones.dev" )
-           ARGS="-proxy-id node1vps1 -wan-address 172.31.245.1"
+           ARGS="-proxy-id node1vps1"
            echo "Node1 ${ARGS}"
            ;;
      "node2.vps1.kristianjones.dev" )
-           ARGS="-proxy-id node2vps1 -wan-address 172.31.245.2"
+           ARGS="-proxy-id node2vps1"
            echo "Node2 ${ARGS}"
            ;;
      "node3.vps1.kristianjones.dev" )
-           ARGS="-proxy-id node3vps1 -wan-address 172.31.245.3"
+           ARGS="-proxy-id node3vps1"
            echo "Node3 ${ARGS}"
            ;;
      * )
