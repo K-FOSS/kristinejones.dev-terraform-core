a2f3bfd481a9c057abc4e0c93c0af116c7c6b2a6
Kristian Jones <me@kristianjones.dev>
2021-09-14 17:05:23 +0000

    fix: Remove manual bind

--- Docker/Services/Consul/Configs/IngressGateway/start.sh
+++ Docker/Services/Consul/Configs/IngressGateway/start.sh
@@ -2,15 +2,15 @@
 
 case ${NODE_HOST} in
      "node1.vps1.kristianjones.dev" )
-           ARGS="-proxy-id node1vps1 -bind-address=node1vps1=0.0.0.0:8443"
+           ARGS="-proxy-id node1vps1"
            echo "Node1 ${ARGS}"
            ;;
      "node2.vps1.kristianjones.dev" )
-           ARGS="-proxy-id node2vps1 -bind-address=node2vps1=0.0.0.0:8443"
+           ARGS="-proxy-id node2vps1"
            echo "Node2 ${ARGS}"
            ;;
      "node3.vps1.kristianjones.dev" )
-           ARGS="-proxy-id node3vps1 -bind-address=node3vps1=0.0.0.0:8443"
+           ARGS="-proxy-id node3vps1"
            echo "Node3 ${ARGS}"
            ;;
      * )
